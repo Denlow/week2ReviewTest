539183cc39dbb238842847cd039ed04ae2f66778
Dave Denlow and Erik Killops <dave@>
2016-10-20 16:21:52 -0700

    change all functions completely, add background color, hide current picture before reload

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,67 @@
+// var intoMen = false;
+// var intoWomen = false;
+// var intoYoung = false;
+// var intoMature = false;
 
-var intoMen;
-var intoWomen;
-var intoYoung;
-var intoMature;
+// var checkMatch = function() {
+//   if ($("#men").is(':checked')) {
+//       intoMen = true;
+//   }
+//   if ($("#women").is(':checked')) {
+//       intoWomen = true;
+//   }
+//   if ($("#young").is(':checked')) {
+//       intoYoung = true;
+//   }
+//   if ($("#mature").is(':checked')) {
+//       intoMature = true;
+//   }
+// };
 
-var checkMatch = function() {
-  if ($("#men").is(':checked')) {
-    intoMen = true;
+// BACK-END
+
+var getResults = function(intoMen, intoWomen, intoYoung, intoMature) {
+  if (intoMen && intoYoung) {
+    return "hartnett";
   }
-  if ($("#women").is(':checked')) {
-      intoWomen = true;
+  else if (intoMen && intoMature) {
+    return "rourke";
   }
-  if ($("#young").is(':checked')) {
-      intoYoung = true;
+  else if (intoWomen && intoYoung) {
+    return "hath";
   }
-  if ($("#mature").is(':checked')) {
-      intoMature = true;
+  else if (intoWomen && intoMature) {
+    return "streep";
   }
 };
 
-var displayMatch = function(intoMen, intoWomen, intoYoung, intoMature) {}
+
+// FRONT-END
+
+var displayOutput = function(celeb) {
+  $("#streep, #hartnett, #rourke, #hath").hide();
+  $("#" + celeb).show();
+};
 
 $(document).ready(function(){
   $("button#match").click(function(event){
-    checkMatch()
+
+    if ($("#men").is(':checked')) {
+        var intoMen = true;
+    }
+    if ($("#women").is(':checked')) {
+        var intoWomen = true;
+    }
+    if ($("#young").is(':checked')) {
+        var intoYoung = true;
+    }
+    if ($("#mature").is(':checked')) {
+        var intoMature = true;
+    }
+
+    var celeb = getResults(intoMen, intoWomen, intoYoung, intoMature);
+
+    displayOutput(celeb)
 
   event.preventDefault();
   });
