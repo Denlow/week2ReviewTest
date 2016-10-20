49a8999f47fa72a931f9dfece566ae8e5e7e6dbe
Dave Denlow and Erik Killops <dave@>
2016-10-20 14:46:31 -0700

    add checkMatch function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+
+var intoMen;
+var intoWomen;
+var intoYoung;
+var intoMature;
+
+var checkMatch = function() {
+  if ($("#men").is(':checked')) {
+    intoMen = true;
+  }
+  if ($("#women").is(':checked')) {
+      intoWomen = true;
+  }
+  if ($("#young").is(':checked')) {
+      intoYoung = true;
+  }
+  if ($("#mature").is(':checked')) {
+      intoMature = true;
+  }
+};
+
+var displayMatch = function(intoMen, intoWomen, intoYoung, intoMature) {}
+
 $(document).ready(function(){
-  
+  $("button#match").click(function(event){
+    checkMatch()
+
+  event.preventDefault();
+  });
+
 });
